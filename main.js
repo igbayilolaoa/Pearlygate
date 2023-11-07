50a34aecc3cd2ce2ec92643c8434a47e7051b955
igbayilolaoa <olamilekan.igbayilola@hotmail.com>
2023-11-07 16:58:06 +0000

    pearlygate website

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// stateTextElements.forEach(element => {
+//     const stateTextElements = document.querySelectorAll(".state-text");
+//     // const card = element.closest('.card'); // Get the parent card of the "State" paragraph
+
+//     // Add event listeners to each "State" paragraph
+//     element.addEventListener('mouseover', () => {
+//         stateTextElements.style.display = "block";
+//     });
+
+//     element.addEventListener('mouseout', () => {
+//         stateTextElements.style.display = "none";
+//     });
+// });
+
+// const closeBtn = (event) =>{
+//     event.target.style.display = "none";
+//     console.log("hello")
+// }
